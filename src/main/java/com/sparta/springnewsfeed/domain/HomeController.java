90cb83f77aa6d47cc6b10c0abba75414a1c9c01d
junxtar <aa1870088@naver.com>
2023-11-21 22:42:37 +0900

    feat: 회원가입 페이지 구현

--- src/main/java/com/sparta/springnewsfeed/domain/HomeController.java
+++ src/main/java/com/sparta/springnewsfeed/domain/HomeController.java
@@ -1,5 +1,14 @@
 package com.sparta.springnewsfeed.domain;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
 
 }
