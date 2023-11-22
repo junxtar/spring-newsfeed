71e9f002a255e4cbaf35cc0e9aa63e6a168b7152
BMDK9 <qnrakdlzm77@naver.com>
2023-11-22 17:37:45 +0900

    fix : 연관 관계 수정

--- src/main/java/com/sparta/springnewsfeed/domain/post/entity/Post.java
+++ src/main/java/com/sparta/springnewsfeed/domain/post/entity/Post.java
@@ -1,21 +1,16 @@
 package com.sparta.springnewsfeed.domain.post.entity;
 
+import com.sparta.springnewsfeed.domain.comment.entity.Comment;
 import com.sparta.springnewsfeed.domain.post.dto.PostRequestDto;
 import com.sparta.springnewsfeed.domain.user.entity.User;
 import com.sparta.springnewsfeed.global.util.BaseTime;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,6 +20,7 @@ import lombok.Setter;
 public class Post extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
     private  Long id;
 
     @Column(nullable = false)
@@ -36,6 +32,9 @@ public class Post extends BaseTime {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    List<Comment> commentList;
 
 
     public Post (PostRequestDto requestDto, User user) {
