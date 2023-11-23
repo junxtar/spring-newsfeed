c77faee0db98646e742e530d8d541733e103417f
junxtar <aa1870088@naver.com>
2023-11-23 12:10:05 +0900

    [#15] refactor: 성공 메세지 변경

--- src/main/java/com/sparta/springnewsfeed/global/common/CommonCode.java
+++ src/main/java/com/sparta/springnewsfeed/global/common/CommonCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CommonCode {
-    OK("회원 가입을 완료했습니다!", HttpStatus.OK.value());
+    OK("성공", HttpStatus.OK.value());
+
     private final String message;
     private final Integer code;
 
